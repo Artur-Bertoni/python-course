5b2e80168fe352fce781f4aa2961b696c239a144
arturSantos <artur.santos@southsystem.com.br>
2023-02-28 18:01:30 -0300

    Praticticing with Strings and Integers

---
+++ basicPython/praticticingWithStringsAndIntegers.py
@@ -0,0 +1,8 @@
+nome = 'Artur'
+idade = 22
+idade = str(idade)
+cidade = 'Porto Alegre'
+
+print('O ' + nome + ' tem ' + idade + ' anos de idade e mora em ' + cidade + '.')
+
+# O Artur tem 22 anos de idade e mora em Porto Alegre.
