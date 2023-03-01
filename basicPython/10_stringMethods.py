831a7a8957c310f34211c5ae6a0f98dd7970d8ed
arturSantos <artur.santos@southsystem.com.br>
2023-03-01 13:30:17 -0300

    String Methods

---
+++ basicPython/10_stringMethods.py
@@ -0,0 +1,10 @@
+
+message = '   Eu adoro comida Caseira'
+#index     0123456789
+
+print('Lower: ' + message.lower())
+print('Upper: ' + message.upper())
+print('Capitalize: ' + message.capitalize())
+print('Find: ' + str(message.find('adoro')))
+print('Replace: ' + message.replace('Caseira', 'feita em casa'))
+print('Strip: ' + message.strip())
