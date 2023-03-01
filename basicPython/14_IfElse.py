d28303bce29bbdee3b3f4923fd4dd0525d97e5e0
arturSantos <artur.santos@southsystem.com.br>
2023-03-01 15:50:12 -0300

    If, Else

---
+++ basicPython/14_IfElse.py
@@ -0,0 +1,10 @@
+speed = 120
+
+if speed > 110:
+    print('Acima da velocidade permitida')
+    print('Favor reduzir a sua velocidade')
+elif speed < 60:
+    print('Favor dirigir acima de 80Km/h')
+else:
+    print('Velocidade OK')
+
