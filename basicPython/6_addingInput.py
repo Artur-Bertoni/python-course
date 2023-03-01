99fc529c343d8659e048e954dc6574695381afef
arturSantos <artur.santos@southsystem.com.br>
2023-03-01 11:38:16 -0300

    Adding Input

---
+++ basicPython/6_addingInput.py
@@ -0,0 +1,8 @@
+nome = input('Qual é o seu nome: ')
+idade = input('Qual é a sua idade: ')
+idade = str(idade)
+cidade = input('Onde você mora: ')
+
+print('O ' + nome + ' tem ' + idade + ' anos de idade e mora em ' + cidade + '.')
+
+# O Artur tem 22 anos de idade e mora em Porto Alegre.
