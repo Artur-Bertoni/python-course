e0ed0f608fd393c1b03db475cd0480f81d1ab7db
arturSantos <artur.santos@southsystem.com.br>
2023-03-03 15:48:34 -0300

    Logical Operators - v1

--- basicPython/14_IfElse.py
+++ basicPython/14_IfElse.py
@@ -1,9 +1,9 @@
-speed = 120
+velocity = 120
 
-if speed > 110:
+if velocity > 110:
     print('Acima da velocidade permitida')
     print('Favor reduzir a sua velocidade')
-elif speed < 60:
+elif velocity < 60:
     print('Favor dirigir acima de 80Km/h')
 else:
     print('Velocidade OK')
---
+++ basicPython/15_logicalOperators.py
@@ -0,0 +1,9 @@
+# Logical Operators
+
+incomeAbove5Thousand = True
+cleanName = True
+
+if incomeAbove5Thousand and cleanName:
+    print('Financiamento Aprovado')
+else:
+    print('Financiamento Negado')
