0e1fc5232bd1e6b76621d56d5a9c23d1a1a95482
arturSantos <artur.santos@southsystem.com.br>
2023-03-08 08:45:54 -0300

    Logical Operators - v2

--- basicPython/15_logicalOperators.py
+++ basicPython/15_logicalOperators.py
@@ -1,9 +1,9 @@
 # Logical Operators
 
-incomeAbove5Thousand = True
-cleanName = True
+incomeAbove5Thousand = False
+cleanName = False
 
-if incomeAbove5Thousand and cleanName:
+if incomeAbove5Thousand or cleanName:
     print('Financiamento Aprovado')
 else:
     print('Financiamento Negado')
